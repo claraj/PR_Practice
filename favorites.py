d2a8468ab4276a3511f3246eeb8dcbe71fa9e4e7
clara <10088152+claraj@users.noreply.github.com>
2022-09-08 15:28:25 -0500

    replace markdown file with python version

---
+++ favorites.py
@@ -0,0 +1,61 @@
+"""A collaborative list of some of ITEC 2905's favorite things."""
+
+# Favorite Color
+
+favorite_color = ''
+print(f'One of ITEC 2905s favorite colors is {favorite_color}')
+
+# Favorite Food
+
+favorite_food = ''
+print(f'One of ITEC 2905s favorite foods is {favorite_food}')
+
+# Favorite Movie or TV Show
+
+favorite_movie = ''
+print(f'One of ITEC 2905s favorite movies is {favorite_movie}')
+
+## Favorite Music
+
+favorite_music = ''
+print(f'One of ITEC 2905s favorite musics is {favorite_music}')
+
+## Favorite Place
+
+favorite_place = ''
+print(f'One of ITEC 2905s favorite places is {favorite_place}')
+
+## Favorite Holiday
+
+favorite_holiday = ''
+print(f'One of ITEC 2905s favorite holidays is {favorite_holiday}')
+
+## Favorite Book
+
+favorite_book = ''
+print(f'One of ITEC 2905s favorite books is {favorite_book}')
+
+## Favorite Animal
+
+favorite_animal = ''
+print(f'One of ITEC 2905s favorite colors is {favorite_animal}')
+
+## Favorite Weather
+
+favorite_weather = ''
+print(f'One of ITEC 2905s favorite type of weather is {favorite_weather}')
+
+## Favorite Drink
+
+favorite_drink = ''
+print(f'One of ITEC 2905s favorite drinks is {favorite_drink}')
+
+## Favorite Store
+
+favorite_store = ''
+print(f'One of ITEC 2905s favorite stores is {favorite_store}')
+
+## Favorite Sport
+
+favorite_sport = ''
+print(f'One of ITEC 2905s favorite sports is {favorite_sport}')
